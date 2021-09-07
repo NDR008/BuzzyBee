37ae7200ea736c8ce3959ee846e9091af093e66e
Nadir Zaman Syed <nadir.syedsammut@gmail.com>
2021-09-07 22:04:04 +0200

    doukutsupsx

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ TYPE = ps-exe
 
 SRCS = main.c \
 engine/basics.c \
-engine/pads.c \
+engine/input.c \
 ../third_party/common/syscalls/printf.s \
 ../third_party/common/crt0/crt0.s \
 
--- main.c
+++ main.c
@@ -38,7 +38,7 @@ SOFTWARE.
 #include <libgs.h>
 #include "libs/images.h"
 #include "engine/basics.h"
-#include "engine/pads.h"
+#include "engine/input.h"
 
 // Global system
 #define OT_LENGTH 1
