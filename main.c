011f1cfdf7b19db606d0b447288f8270f22555ff
Nadir Zaman Syed <nadir.syedsammut@gmail.com>
2021-09-19 01:49:28 +0200

    working intro

--- main.c
+++ main.c
@@ -79,7 +79,7 @@ AnimatedObject Buzzy;
 Image BuzzyL[1];
 
 AnimatedObject jam;
-Image jamL[1];
+Image jamL[3];
 
 
 void initialize();
@@ -111,6 +111,7 @@ void initialize() {
 #define SPRITEHEIGHT 30
 #define GROUND (SCREEN_HEIGHT-20-30) * factor
 #define CEILING (SPRITEHEIGHT)
+int a, b, c = 0; // a is for bringing in Buzzy, b for bringing in Bee, c from bringing in Jam
 
 int gameState = 0; // 0 = start state, 1 = play state, 2 = pause
 
@@ -146,8 +147,38 @@ void updateAnimation(){
 }
 
 void gameScreen(){
-    animate(&pressStart);
+        printf("%i, %i, %i\n", a,b,c);
+    int Buzzy_limit = (BuzzyL[0].sprite.w / 2) * factor;
+    if (Buzzy.x_pos < Buzzy_limit) {
+        Buzzy.x_pos += Buzzy.x_vel;
+    }
+    else {    
+        a = 1;
+    }
+
+    int Bee_limit = (SCREEN_WIDTH/2 + BeeL[0].sprite.w / 2 + 10) * factor;
+    if (Bee.x_pos > Bee_limit) {
+        Bee.x_pos += Bee.x_vel;    
+    }
+    else {
+        b = 1;
+    }
+    if (a + b == 2) {
+        int jam_limit = 150 * factor;
+        if (jam.y_pos > jam_limit) {
+            jam.y_pos += jam.y_vel;    
+        }
+        else {
+            c = 1;
+        }
+        animate(&jam);
+    }
+
+    if (c) {
+        animate(&pressStart);
+    }
     animate(&Bee);
+    animate(&Buzzy);
 }
 
 void animate(AnimatedObject *animatedObj){
@@ -188,25 +219,42 @@ void initIntro(){
     pressStartL[2] = createImage(img_Press_start3);
     pressStart.img_list = pressStartL;
 
-    Bee.total_frames = 1; // third frame is reserved
-    Bee.frame_n = 0;
-    Bee.index = 0;
-    Bee.y_pos = (SCREEN_HEIGHT * 1 / 4) * factor;
-    Bee.x_pos = (SCREEN_WIDTH * 3 / 4) * factor;
-    Bee.y_vel, Bee.x_vel = 0;
-    Bee.anim_rate = 25;
-    BeeL[0] = createImage(img_Bee);
-    Bee.img_list = BeeL;
 
     Buzzy.total_frames = 1; 
     Buzzy.frame_n = 0;
     Buzzy.index = 0;
-    Buzzy.y_pos = (SCREEN_HEIGHT * 1 / 4) * factor;
-    Buzzy.x_pos = (SCREEN_WIDTH * 1 / 4) * factor;
-    Buzzy.y_vel, Buzzy.x_vel = 0;
-    Buzzy.anim_rate = 25;
+    Buzzy.x_pos = (5) * factor;
+    Buzzy.y_vel = 0;
+    Buzzy.x_vel = 3 * factor;
+    Buzzy.anim_rate = 1;
     BuzzyL[0] = createImage(img_Buzzy);
+    Buzzy.y_pos = (BuzzyL[0].sprite.h / 3) * factor;
+    Buzzy.x_pos = (-BuzzyL[0].sprite.w/2) * factor;
     Buzzy.img_list = BuzzyL;
+
+    Bee.total_frames = 1; // third frame is reserved
+    Bee.frame_n = 0;
+    Bee.index = 0;
+    Bee.y_vel = 0;
+    Bee.x_vel = -2 * factor;
+    Bee.anim_rate = 1;
+    BeeL[0] = createImage(img_Bee);
+    Bee.y_pos = (BeeL[0].sprite.h) * factor;
+    Bee.x_pos = (SCREEN_WIDTH + BeeL[0].sprite.w/2) * factor;
+    Bee.img_list = BeeL;
+
+    jam.total_frames = 3; // third frame is reserved
+    jam.frame_n = 0;
+    jam.index = 0;
+    jam.y_vel = -1 * factor;
+    jam.x_vel = 0;
+    jam.anim_rate = 10;
+    jamL[0] = createImage(img_jam_1);
+    jamL[1] = createImage(img_jam_2);
+    jamL[2] = createImage(img_jam_3);
+    jam.y_pos = SCREEN_HEIGHT * factor;
+    jam.x_pos = (SCREEN_WIDTH / 2) * factor;
+    jam.img_list = jamL;
 }
 
 int main() {
